06d9c04a449de3cbe2ac47ed28c175f4dc3c8a5f
Martin Bavio <mbavio@gmail.com>
2020-09-22 23:33:40 -0300

    Add page title

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,20 @@
+import Head from 'next/head';
 import Header from '../Header';
 import Footer from '../Footer';
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className="antialiased">
-      <Header />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>FRONTENDCAFE</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="antialiased">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 };
 
