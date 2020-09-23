799fd546350ed611b3e1ef038ca08a0883220e73
Martin Bavio <mbavio@gmail.com>
2020-09-23 00:30:51 -0300

    Pero el TOC es mas fuerte 🎵

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -6,11 +6,11 @@ const HomeHero: React.FC = () => {
       <section className={`bg-cover text-gray-700 body-font ${styles.main}`}>
         <div className="flex bg-cover px-5 py-16 items-center justify-center flex-wrap">
           <div className="md:w-1/3 px-16 lg:min-w-0 sm:min-w-full md:py-16 py-10">
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-3xl mb-5">
               ¡Somos una comunidad abierta de{' '}
               <span className="text-green-500">programación!</span>
             </h1>
-            <p className="text-gray-200 text-lg">
+            <p className="text-gray-200 text-lg leading-relaxed">
               Aprender a programar es difícil, ¿no? Utilizamos este espacio en
               Discord para aprender en compañía, colaborar en proyectos abiertos
               y conectar con mentores voluntarios.
